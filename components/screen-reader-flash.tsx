82d1a2ebdb09ee7e6d041aa62e4d38fd81338e58
David Woodward <dw@Ruis-MacBook-Pro.local>
2022-09-04 21:08:25 -0400

    Translation tweaks

--- components/screen-reader-flash.tsx
+++ components/screen-reader-flash.tsx
@@ -4,7 +4,7 @@ import { screenReaderFlashAtom } from "../store";
 export default function ScreenReaderFlash() {
   const [flash, _setFlash] = useAtom(screenReaderFlashAtom);
   return (
-    <section aria-live="polite" className="visually_hidden">
+    <section aria-live="polite" aria-atomic="true" className="visually_hidden">
       {flash}
     </section>
   );
